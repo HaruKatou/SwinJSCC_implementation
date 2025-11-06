6087913547b3f0b99adc616fcd88cb57a87aedec
HaruKatou <bqtrung1001.job@gmail.com>
2025-11-06 18:18:45 +0700

    Initial commit

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.0"
+__author__ = "HaruKatou"
+__email__ = "bqtrung1001.job@gmail.com"
+
+from .data import *
+from .models import *
+from .training import *
+from .evaluation import *
+from .utils import *
